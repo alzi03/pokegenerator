cdec508805e88d08ad78645d9808d424109aa1a2
alzi03 <alammzishan03@gmail.com>
2023-07-18 23:50:37 -0400

    api request + json format

--- randomgen/views.py
+++ randomgen/views.py
@@ -10,6 +10,11 @@ from django.views import generic
 from django.urls import reverse
 from django.utils.safestring import mark_safe
 
+import random
+import requests
+import pokebase as pb
+import pprint
+
 # Number of Available Pokemon, established for PokeAPI endpoint
 numPokemon = 1010
 
@@ -23,6 +28,27 @@ class IndexView(generic.TemplateView):
     '''
     template_name = "randomgen/home.html"
     
+    
     def get_context_data(self, **kwargs):
-        context = self.get_context_data()
+        context = super().get_context_data(**kwargs)
+        
+        pokemon = self.get_pokemon_image()
+        print(context)
         return context
+
+    def get_pokemon_image(self):
+        id = random.randint(0, numPokemon)
+        # Argument: id
+        pokemon = pb.pokemon(id_or_name=id)
+        
+        json = {
+            'id': pokemon.id,
+            'sprite': pokemon.sprites.front_default
+        }
+        
+        return json
+        
+        
+        
+    def post(self, request, **kwargs):
+        return
